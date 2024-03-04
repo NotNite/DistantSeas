a2bd2e021e365304539ec78ae2cbbdf3a2f453ef
chirp <72366111+chirpxiv@users.noreply.github.com>
2024-03-04 08:47:07 +0000

    Fix crash when dragging tables offscreen

--- DistantSeas/Windows/Main/JournalSection.cs
+++ DistantSeas/Windows/Main/JournalSection.cs
@@ -4,6 +4,7 @@ using System.IO;
 using CheapLoc;
 using Dalamud.Interface;
 using Dalamud.Interface.Colors;
+using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
 using Dalamud.Logging;
 using DistantSeas.Common;
@@ -53,53 +54,54 @@ public class JournalSection : MainWindowSection {
             this.UpdateJournalEntries();
         }
 
-        var flags = ImGuiTableFlags.Borders
+        const ImGuiTableFlags flags = ImGuiTableFlags.Borders
                     | ImGuiTableFlags.Resizable
                     | ImGuiTableFlags.ScrollY
                     | ImGuiTableFlags.RowBg;
 
-        using (ImRaii.Table("##DistantSeasJournal", 6, flags)) {
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionDate", "Date"));
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionRoute", "Route"));
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionTime", "Time"));
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionPoints", "Points"));
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionSpectrals", "Spectrals"));
-            ImGui.TableSetupColumn(Loc.Localize("JournalSectionButtons", "Buttons"));
-            ImGui.TableHeadersRow();
+        using var table = ImRaii.Table("##DistantSeasJournal", 6, flags);
+        if (!table.Success) return;
+            
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionDate", "Date"));
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionRoute", "Route"));
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionTime", "Time"));
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionPoints", "Points"));
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionSpectrals", "Spectrals"));
+        ImGui.TableSetupColumn(Loc.Localize("JournalSectionButtons", "Buttons"));
+        ImGui.TableHeadersRow();
 
-            foreach (var entry in this.journalEntries!) {
-                ImGui.TableNextRow();
-                ImGui.TableNextColumn();
+        foreach (var entry in this.journalEntries!) {
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn();
 
-                // 2023-01-01 12:00:00 PM
-                var dateStr = entry.Time.ToString("yyyy-MM-dd hh:mm:ss tt");
-                ImGui.TextUnformatted(dateStr);
-                ImGui.TableNextColumn();
+            // 2023-01-01 12:00:00 PM
+            var dateStr = entry.Time.ToString("yyyy-MM-dd hh:mm:ss tt");
+            ImGui.TextUnformatted(dateStr);
+            ImGui.TableNextColumn();
 
-                ImGui.TextUnformatted(entry.DestinationName);
-                ImGui.TableNextColumn();
+            ImGui.TextUnformatted(entry.DestinationName);
+            ImGui.TableNextColumn();
 
-                ImGui.TextUnformatted(Utils.TimeName(entry.DestinationTime));
-                ImGui.TableNextColumn();
+            ImGui.TextUnformatted(Utils.TimeName(entry.DestinationTime));
+            ImGui.TableNextColumn();
 
-                ImGui.TextUnformatted(entry.TotalPoints.ToString());
-                ImGui.TableNextColumn();
+            ImGui.TextUnformatted(entry.TotalPoints.ToString());
+            ImGui.TableNextColumn();
 
-                ImGui.TextUnformatted(entry.Spectrals.ToString());
-                ImGui.TableNextColumn();
+            ImGui.TextUnformatted(entry.Spectrals.ToString());
+            ImGui.TableNextColumn();
 
-                var enabled = ImGui.GetIO().KeyCtrl;
+            var enabled = ImGui.GetIO().KeyCtrl;
 
-                if (Utils.DisabledButtonWithTooltip(
-                        !enabled,
-                        Loc.Localize("JournalSectionButtonDelete", "Delete"),
-                        disabledText: Loc.Localize("JournalSectionButtonDeleteTooltip",
-                                                   "This can't be undone. Hold Control to enable this button.")
-                    )) {
-                    if (enabled) {
-                        File.Delete(entry.Path);
-                        this.UpdateJournalEntries();
-                    }
+            if (Utils.DisabledButtonWithTooltip(
+                    !enabled,
+                    Loc.Localize("JournalSectionButtonDelete", "Delete"),
+                    disabledText: Loc.Localize("JournalSectionButtonDeleteTooltip",
+                                               "This can't be undone. Hold Control to enable this button.")
+                )) {
+                if (enabled) {
+                    File.Delete(entry.Path);
+                    this.UpdateJournalEntries();
                 }
             }
         }
